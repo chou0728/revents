83fc2b92c952d5d7007d7e6720c25aff655d1bd7
chou <eric.chou@cloud-interactive.com>
2018-12-19 01:22:30 +0800

    change to ES7 arrow function component

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function UserDetailedPage() {
+const UserDetailedPage = () => {
   return (
     <div>
       <h1>UserDetailedPage</h1>
     </div>
   );
-}
+};
 
 export default UserDetailedPage;
