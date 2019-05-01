75a9262f558a1d4fef6e1e5a53ebabb101a177ba
chou0728 <dean34520@gmail.com>
2019-05-01 13:22:43 +0800

    Return a function instead in authActions

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,11 +1,23 @@
 import { LOGIN_USER, SIGN_OUT_USER } from './authConstants'
+import { closeModal } from '../modals/modalActions'
 
+
+//在action中return function (在function中直接dispatch)
+// return的function做了兩件事情 1. dispatch login  2.closeModal
 export const login = (creds) => {
-  return {
-    type: LOGIN_USER,
-    payload: creds
+  return dispatch => {
+    dispatch({ type: LOGIN_USER, payload: { creds }})
+    dispatch(closeModal())
   }
 }
+
+
+// export const login = (creds) => {
+//   return {
+//     type: LOGIN_USER,
+//     payload: {creds}
+//   }
+// }
 
 export const logout = () => {
   return {
