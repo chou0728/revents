6ecc2e80fa5d35c5f7d2d32f0082416bb1294090
chou0728 <dean34520@gmail.com>
2019-05-05 23:43:28 +0800

    Add EventActivity component

---
+++ src/features/event/EventActivity/EventActivity.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header, Segment} from 'semantic-ui-react'
+
+const EventActivity = () => {
+  return (
+    <div>
+      <Header attached='top' content='Recnt Activity'/>
+      <Segment attached>
+        <p>Recnet Activity</p>
+      </Segment>
+    </div>
+  )
+}
+
+export default EventActivity
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -4,6 +4,7 @@ import { Grid } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import { deleteEvent } from '../eventReducer';
 import LoadingComponent from '../../../app/layout/LoadingComponent'
+import EventActivity from '../EventActivity/EventActivity'
 
 const mapState = (state) => ({
   events: state.events,
@@ -36,6 +37,7 @@ class EventDashboard extends Component {
           />
         </Grid.Column>
         <Grid.Column width={6}>
+          <EventActivity/>
         </Grid.Column>
       </Grid>
     );
